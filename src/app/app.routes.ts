b0eda3d15f800175897469511cddb38365715560
malaka96 <madhubhashana655@gmail.com>
2026-01-16 01:29:47 +0530

    created contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Projects } from './pages/projects/projects';
 import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: "about",
         component:About
+    },
+    {
+        path: "contact",
+        component: Contact
     }
 ];
