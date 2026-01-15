0a4d882cc56464a744104febe258361d87c22328
malaka96 <madhubhashana655@gmail.com>
2026-01-16 00:56:55 +0530

    created home page

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  menuOpen = false;
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
